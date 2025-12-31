2614f0cf43386428e13061e90bbbf96bc7bf37b6
Pavan Y N <pavannagu22@gmail.com>
2025-12-31 20:33:51 +0000

    this is the logo

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,16 +1,18 @@
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <Link
-      href="/"
-      className={cn(
-        'text-2xl font-bold font-headline tracking-tighter text-primary hover:text-primary/90 transition-colors',
-        className
-      )}
-    >
-      Ritzee Wear
+    <Link href="/" className={cn('relative block h-10 w-40', className)}>
+      <Image
+        src="https://storage.googleapis.com/aifirebase-113b.appspot.com/user-uploads/L2FwcC1wcm90b3R5cGVyL3VzZXI6Y2ljcC1hbmRyZS1kZXYvMTcwYjNlZTYtMDI5MC00NDhhLTkzZWQtMDc0MGM2YzI3ZjEz.png"
+        alt="Ritzee Wear Logo"
+        fill
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        className="object-contain"
+        priority
+      />
     </Link>
   );
 }
