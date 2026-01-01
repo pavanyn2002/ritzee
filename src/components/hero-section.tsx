d345f44e153de828be543781d83ed4410d6511c2
Pavan Y N <pavannagu22@gmail.com>
2026-01-01 07:27:59 +0000

    remove the animation. lets do from first since you are not able to debug

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -12,20 +12,9 @@ import {
   CarouselItem,
 } from '@/components/ui/carousel';
 import Autoplay from 'embla-carousel-autoplay';
-import React, { useState, useEffect } from 'react';
-import { cn } from '@/lib/utils';
-
+import React from 'react';
 
 export default function HeroSection() {
-    const [runLoadAnimation, setRunLoadAnimation] = useState(true);
-    
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setRunLoadAnimation(false);
-        }, 3000);
-        return () => clearTimeout(timer);
-    }, []);
-
   const heroImages = [
     ...products.map(p => ({id: p.id, image: p.image, name: p.name, imageHint: p.imageHint})),
     {
@@ -81,10 +70,7 @@ export default function HeroSection() {
         <div className="flex flex-col items-center space-y-6">
           <ScrollAnimation>
              <h1 
-              className={cn(
-                'text-6xl md:text-8xl lg:text-9xl font-bold font-headline glitch',
-                {'glitch-load': runLoadAnimation}
-              )}
+              className='text-6xl md:text-8xl lg:text-9xl font-bold font-headline'
               data-text="RITZEE"
             >
               RITZEE
